9d8e6f4b415b9242333e69efc0c7cc72ec2afe58
yuboyang <yuboyang@momenta.ai>
2021-09-24 11:37:08 +0800

    test

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -1,19 +1,35 @@
 import * as React from 'react'
+import { useEffect ,useState} from 'react';
 
 export interface Props {
     name: string;
     level?: number;
 }
 
-const get = (num : number) => {
+const get = (num: number) => {
     return Array(num + 1).join('!');
 }
 const Demo: React.FC<Props> = ({ name, level = 1 }: Props) => {
 
-    console.log(process.env)
+    const [query, setQuery] = useState<any>("222333");
+    
+    const makeData = (x :any) => {
+        console.log('aa',x)
+        return x+1;
+    }
+
+    useEffect(() => {
+        const data = async () => {
+            const res = await makeData(9)
+            console.log(123)
+            setQuery(res)
+        }
+        data()
+    },[])
+    console.log(query)
 
     return (
-        <div style={{ height:'200px' }} className="greeting">
+        <div style={{ height: '200px' }} className="greeting">
             <h1>Hello {name + get(level)}</h1>
         </div>
     )
